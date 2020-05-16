6e3ae137586f3b89e17ffe5fe302cded81f96331
Praveen Bisht <prvnbist@gmail.com>
2020-05-16 16:27:25 +0530

    feat: realm env

--- src/store/auth/index.js
+++ src/store/auth/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Keycloak from 'keycloak-js'
 
 const keycloak = new Keycloak({
-   realm: 'accounts',
+   realm: process.env.REACT_APP_KEYCLOAK_REALM,
    url: process.env.REACT_APP_KEYCLOAK_URL,
    clientId: process.env.REACT_APP_KEYCLOAK_CLIENT,
    'ssl-required': 'none',
